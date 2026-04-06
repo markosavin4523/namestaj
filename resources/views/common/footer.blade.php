4ac08e7a483be6c2b4ec671a68accfcc337cee30
Mare Mare <maremare@Mares-MacBook-Air.local>
2026-04-06 17:00:57 +0200

    dokumentacija

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -17,13 +17,9 @@
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Dokumentacija</a></li>
             </ul>
         </div>
-        <div class="col mb-3"><h5>Stranice</h5>
+        <div class="col mb-3"><h5>Dokumentaicja</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                <li class="nav-item mb-2"><a href="{{ asset("dokumentacija.pdf") }}" class="nav-link p-0 text-body-secondary">Dokumentacija</a></li>
             </ul>
         </div>
     </footer>
